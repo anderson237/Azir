043ef15809d510cd385ef2d15ec9005b735d82f7
patcool <elomopatrick.pn@gmail.com>
2021-04-27 22:14:26 +0100

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,43 @@
 <template>
-  <hello-world />
+  <v-app :style="{background: $vuetify.theme.themes.light.background}">
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+const gradients = [
+  ["#222"],
+  ["#42b3f4"],
+  ["red", "orange", "yellow"],
+  ["purple", "violet"],
+  ["#00c6ff", "#F0F", "#FF0"],
+  ["#f72047", "#ffd200", "#1feaea"],
+  ]
 
 export default {
   name: "Home",
-
-  components: {
-    HelloWorld,
+  data: () => ({
+    width: 2,
+    radius: 10,
+    padding: 8,
+    lineCap: "round",
+    gradient: gradients[5],
+    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+    gradientDirection: "top",
+    gradients,
+    fill: false,
+    type: "trend",
+    autoLineWidth: false,
+    arrayEvents: null,
+    date2: new Date().toISOString().substr(0, 10),
+  }),
+  methods: {
+    functionEvents (date) {
+      const [,, day] = date.split('-')
+      if ([12, 17, 28].includes(parseInt(day, 10))) return true
+      if ([1, 19, 22].includes(parseInt(day, 10))) return ['red', '#00f']
+      return false
+    },
   },
 };
 </script>
