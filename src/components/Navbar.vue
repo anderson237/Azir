b53897c754897d0b01a6b51c3cbfa0b38c76e744
patcool <elomopatrick.pn@gmail.com>
2021-04-28 00:19:48 +0100

    first commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,31 +8,31 @@
       </v-list>
 
       <v-list flat>
-        <v-list-item-content router to="/">
+        <v-list-item-content>
           <v-icon class="mb-2">dashboard</v-icon>
           <v-list-item-subtitle align="center">Dashboard</v-list-item-subtitle>
         </v-list-item-content>
 
-        <v-list-item-content router to="/">
+        <v-list-item-content>
           <v-icon class="mb-2">fas fa-notes-medical</v-icon>
           <v-list-item-subtitle align="center">Doctor</v-list-item-subtitle>
         </v-list-item-content>
 
-        <v-list-item-content router to="/">
+        <v-list-item-content>
           <v-icon class="mb-2">fas fa-user-md</v-icon>
           <v-list-item-subtitle align="center">Contact</v-list-item-subtitle>
         </v-list-item-content>
 
-        <v-list-item-content router to="/">
+        <v-list-item-content>
           <v-icon class="mb-2">fas fa-signature</v-icon>
           <v-list-item-subtitle align="center">Tracker</v-list-item-subtitle>
         </v-list-item-content>
       </v-list>
 
-      <v-list style="bottom:0;" class="ml-3" flat>
+      <v-list style="position: absolute; bottom:0;" flat>
         <v-list-item>
           <v-list-item-action>
-            <v-icon right>fas fa-sign-out-alt</v-icon>
+            <v-icon >fas fa-sign-out-alt</v-icon>
           </v-list-item-action>
         </v-list-item>
       </v-list>
